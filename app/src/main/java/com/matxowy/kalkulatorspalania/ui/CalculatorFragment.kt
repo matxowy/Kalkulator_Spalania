82018fb90c776ebbc78831c48180703c1579fbf7
matxowy <suskamateusz11@gmail.com>
2021-09-25 12:37:59 +0200

    v.1.0 - Created full functional app
    - Added all formulas for calculating fuel consumption, costs and range
    - Added keyboard hiding when changing tab
    - Added setting values in edit text when value was set in previous tab
    - Added channels for showing result in tabs
    
    - Made more transparent result for tabs using bolding
    
    - Prevention for crash when edit text gets null

--- app/src/main/java/com/matxowy/kalkulatorspalania/ui/CalculatorFragment.kt
+++ app/src/main/java/com/matxowy/kalkulatorspalania/ui/CalculatorFragment.kt
@@ -1,20 +1,26 @@
 package com.matxowy.kalkulatorspalania.ui
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.text.bold
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.matxowy.kalkulatorspalania.R
 import com.matxowy.kalkulatorspalania.databinding.CalculatorFragmentBinding
 import com.matxowy.kalkulatorspalania.internal.SelectedTab
+import com.matxowy.kalkulatorspalania.util.StringUtils
+import com.matxowy.kalkulatorspalania.util.roundTo
 import kotlinx.coroutines.flow.collect
 
 class CalculatorFragment : Fragment() {
@@ -45,12 +51,18 @@ class CalculatorFragment : Fragment() {
         binding.rgTypes.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
                 R.id.rb_fuel_consumption -> {
+                    hideSoftKeyboard()
+                    setValuesInEditTexts()
                     makeVisibleFuelConsumptionView()
                 }
                 R.id.rb_costs -> {
+                    hideSoftKeyboard()
+                    setValuesInEditTexts()
                     makeVisibleCostsView()
                 }
-                else -> {
+                R.id.rb_range -> {
+                    hideSoftKeyboard()
+                    setValuesInEditTexts()
                     makeVisibleRangeView()
                 }
             }
@@ -69,25 +81,140 @@ class CalculatorFragment : Fragment() {
                     is CalculatorViewModel.CalculatorEvent.ShowMessageAboutMissingData -> {
                         showMessageAboutMissingData()
                     }
+                    is CalculatorViewModel.CalculatorEvent.ShowResultForConsumptionTab -> {
+                        showResultForConsumptionTab(event.avgConsumption, event.price)
+                    }
+                    is CalculatorViewModel.CalculatorEvent.ShowResultForCostsTab -> {
+                        showResultForCostsTab(event.requiredAmountOfFuel, event.costForTravel, event.costPerPerson)
+                    }
+                    is CalculatorViewModel.CalculatorEvent.ShowResultForRangeTab -> {
+                        showResultForRangeTab(event.amountOfFilledWithFuel, event.rangeInKm)
+                    }
                 }
             }
+        }
+    }
+
+    private fun setValuesInEditTexts() {
+        binding.apply {
+            if(viewModel.refueled != null) etRefueledFromFuelConsumptionTab.setText(
+                StringUtils.trimTrailingZero((viewModel.refueled).toString()))
+            if(viewModel.paid != null) etPaidFromRangeTab.setText(
+                StringUtils.trimTrailingZero((viewModel.paid).toString()))
+            if(viewModel.numberOfPeople != null) etNumberOfPeopleFromCostsTab.setText(
+                StringUtils.trimTrailingZero((viewModel.numberOfPeople).toString()))
+            if(viewModel.kmTraveled != null) etKmTraveledFromFuelConsumptionTab.setText(
+                StringUtils.trimTrailingZero((viewModel.kmTraveled).toString()))
+            if(viewModel.kmTraveled != null) etKmTraveledFromCostsTab.setText(
+                StringUtils.trimTrailingZero((viewModel.kmTraveled).toString()))
+            if(viewModel.fuelPrice != null) etFuelPriceFromRangeTab.setText(
+                StringUtils.trimTrailingZero((viewModel.fuelPrice).toString()))
+            if(viewModel.fuelPrice != null) etFuelPriceFromFuelConsumptionTab.setText(
+                StringUtils.trimTrailingZero((viewModel.fuelPrice).toString()))
+            if(viewModel.fuelPrice != null) etFuelPriceFromCostsTab.setText(
+                StringUtils.trimTrailingZero((viewModel.fuelPrice).toString()))
+            if(viewModel.avgFuelConsumption != null) etAvgFuelConsumptionFromRangeTab.setText(
+                StringUtils.trimTrailingZero((viewModel.avgFuelConsumption).toString()))
+            if(viewModel.avgFuelConsumption != null) etAvgFuelConsumptionFromCostsTab.setText(
+                StringUtils.trimTrailingZero((viewModel.avgFuelConsumption).toString()))
+        }
+    }
+
+    private fun hideSoftKeyboard() {
+        val view = activity?.currentFocus
+        view?.let { v ->
+            val imm =
+                activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+        }
+    }
+
+    private fun showResultForRangeTab(amountOfFilledWithFuel: Double, rangeInKm: Double) {
+        val filledText = SpannableStringBuilder()
+            .append("Zatankowano: ")
+            .bold { append(amountOfFilledWithFuel.roundTo(2).toString()) }
+            .bold { append(" litrów") }
+
+        val expectedRangeText = SpannableStringBuilder()
+            .append("Przewidywany zasięg wynosi: ")
+            .bold { append(rangeInKm.roundTo(2).toString()) }
+            .bold { append(" km") }
+
+        binding.apply {
+            tvFilledWithFuel.text = filledText
+            tvFilledWithFuel.visibility = View.VISIBLE
+            tvExpectedRange.text = expectedRangeText
+            tvExpectedRange.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showResultForCostsTab(
+        requiredAmountOfFuel: Double,
+        costForTravel: Double,
+        costPerPerson: Double
+    ) {
+        val requiredAmountText = SpannableStringBuilder()
+            .append("Wymagana ilość paliwa: ")
+            .bold { append(requiredAmountOfFuel.roundTo(2).toString()) }
+            .bold { append(" litrów") }
+
+        val travelCostText = SpannableStringBuilder()
+            .append("Koszt podróży wyniesie: ")
+            .bold { append(costForTravel.roundTo(2).toString()) }
+            .bold { append("zł") }
+
+        val travelCostPerPersonText = SpannableStringBuilder()
+            .append("Na osobę: ")
+            .bold { append(costPerPerson.roundTo(2).toString()) }
+            .bold { append("zł") }
+
+        binding.apply {
+            tvRequiredAmountOfFuel.text = requiredAmountText
+            tvRequiredAmountOfFuel.visibility = View.VISIBLE
+            tvTravelCost.text = travelCostText
+            tvTravelCost.visibility = View.VISIBLE
+            tvTravelCostPerPerson.text = travelCostPerPersonText
+            tvTravelCostPerPerson.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showResultForConsumptionTab(avgConsumption: Double, price: Double) {
+        val avgFuelConsumptionText = SpannableStringBuilder()
+            .append("Srednie spalanie wynosi: ")
+            .bold { append(avgConsumption.roundTo(2).toString()) }
+            .bold{ append(" litrów") }
+
+        val costFor100kmText = SpannableStringBuilder()
+            .append("Cena za przejechanie 100km wynosi: ")
+            .bold { append(price.roundTo(2).toString()) }
+            .bold { append("zł") }
+
+        binding.apply {
+            tvAvgFuelConsumption.text = avgFuelConsumptionText
+            tvAvgFuelConsumption.visibility = View.VISIBLE
+            tvCostOf100km.text = costFor100kmText
+            tvCostOf100km.visibility = View.VISIBLE
         }
     }
 
     private fun showMessageAboutMissingData() {
         binding.apply {
             when {
-                llCostsView.visibility == View.VISIBLE -> {
-                    tvRequiredAmountOfFuel.text = "Uzupełnij wszystkie pola"
-                    tvRequiredAmountOfFuel.visibility = View.VISIBLE
-                }
                 llFuelConsumptionView.visibility == View.VISIBLE -> {
                     tvAvgFuelConsumption.text = "Uzupełnij wymagane pola"
                     tvAvgFuelConsumption.visibility = View.VISIBLE
+                    tvCostOf100km.visibility = View.GONE
+                }
+                llCostsView.visibility == View.VISIBLE -> {
+                    tvRequiredAmountOfFuel.text = "Uzupełnij wszystkie pola"
+                    tvRequiredAmountOfFuel.visibility = View.VISIBLE
+                    tvTravelCost.visibility = View.GONE
+                    tvTravelCostPerPerson.visibility = View.GONE
                 }
                 llRangeView.visibility == View.VISIBLE -> {
                     tvFilledWithFuel.text = "Uzupełnij wszystkie pola"
                     tvFilledWithFuel.visibility = View.VISIBLE
+                    tvExpectedRange.visibility = View.GONE
                 }
             }
         }
@@ -125,7 +252,10 @@ class CalculatorFragment : Fragment() {
         binding.apply {
             etAvgFuelConsumptionFromRangeTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.avgFuelConsumption = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.avgFuelConsumption = p0.toString().toDouble()
+                        else viewModel.avgFuelConsumption = null
+                    }
                     viewModel.currentAvgFuelConsumption.value = viewModel.avgFuelConsumption
                 }
 
@@ -135,7 +265,10 @@ class CalculatorFragment : Fragment() {
 
             etPaidFromRangeTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.paid = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.paid = p0.toString().toDouble()
+                        else viewModel.paid = null
+                    }
                     viewModel.currentPaid.value = viewModel.paid
                 }
 
@@ -145,7 +278,10 @@ class CalculatorFragment : Fragment() {
 
             etFuelPriceFromRangeTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.fuelPrice = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.fuelPrice = p0.toString().toDouble()
+                        else viewModel.fuelPrice = null
+                    }
                     viewModel.currentFuelPrice.value = viewModel.fuelPrice
                 }
 
@@ -159,7 +295,10 @@ class CalculatorFragment : Fragment() {
         binding.apply {
             etAvgFuelConsumptionFromCostsTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.avgFuelConsumption = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.avgFuelConsumption = p0.toString().toDouble()
+                        else viewModel.avgFuelConsumption = null
+                    }
                     viewModel.currentAvgFuelConsumption.value = viewModel.avgFuelConsumption
                 }
 
@@ -169,7 +308,10 @@ class CalculatorFragment : Fragment() {
 
             etKmTraveledFromCostsTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.kmTraveled = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.kmTraveled = p0.toString().toDouble()
+                        else viewModel.kmTraveled = null
+                    }
                     viewModel.currentKmTraveled.value = viewModel.kmTraveled
                 }
 
@@ -179,7 +321,10 @@ class CalculatorFragment : Fragment() {
 
             etFuelPriceFromCostsTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.fuelPrice = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.fuelPrice = p0.toString().toDouble()
+                        else viewModel.fuelPrice = null
+                    }
                     viewModel.currentFuelPrice.value = viewModel.fuelPrice
                 }
 
@@ -189,7 +334,10 @@ class CalculatorFragment : Fragment() {
 
             etNumberOfPeopleFromCostsTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.numberOfPeople = p0.toString().toInt()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.numberOfPeople = p0.toString().toInt()
+                        else viewModel.numberOfPeople = null
+                    }
                     viewModel.currentNumberOfPeople.value = viewModel.numberOfPeople
                 }
 
@@ -203,7 +351,10 @@ class CalculatorFragment : Fragment() {
         binding.apply {
             etRefueledFromFuelConsumptionTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.refueled = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.refueled = p0.toString().toDouble()
+                        else viewModel.refueled = null
+                    }
                     viewModel.currentRefueled.value = viewModel.refueled
                 }
 
@@ -213,7 +364,10 @@ class CalculatorFragment : Fragment() {
 
             etKmTraveledFromFuelConsumptionTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.kmTraveled = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.kmTraveled = p0.toString().toDouble()
+                        else viewModel.kmTraveled = null
+                    }
                     viewModel.currentKmTraveled.value = viewModel.kmTraveled
                 }
 
@@ -223,7 +377,10 @@ class CalculatorFragment : Fragment() {
 
             etFuelPriceFromFuelConsumptionTab.addTextChangedListener(object : TextWatcher {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.fuelPrice = p0.toString().toDouble()
+                    if (p0 != null) {
+                        if (p0.isNotEmpty()) viewModel.fuelPrice = p0.toString().toDouble()
+                        else viewModel.fuelPrice = null
+                    }
                     viewModel.currentFuelPrice.value = viewModel.fuelPrice
                 }
 
--- app/src/main/java/com/matxowy/kalkulatorspalania/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/matxowy/kalkulatorspalania/ui/CalculatorViewModel.kt
@@ -1,6 +1,5 @@
 package com.matxowy.kalkulatorspalania.ui
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -66,23 +65,42 @@ class CalculatorViewModel : ViewModel() {
     private fun doCalculationInConsumptionTab() = viewModelScope.launch {
         if (refueled == null || kmTraveled == null || fuelPrice == null) {
             calculatorEventChannel.send(CalculatorEvent.ShowMessageAboutMissingData)
+        } else {
+            val avgConsumption = refueled!! / kmTraveled!! * 100
+            val priceFor100km = avgConsumption * fuelPrice!!
+
+            calculatorEventChannel.send(CalculatorEvent.ShowResultForConsumptionTab(avgConsumption, priceFor100km))
         }
     }
 
     private fun doCalculationInCostsTab() = viewModelScope.launch {
         if (avgFuelConsumption == null || kmTraveled == null || fuelPrice == null || numberOfPeople == null) {
             calculatorEventChannel.send(CalculatorEvent.ShowMessageAboutMissingData)
+        } else {
+            val requiredAmountOfFuel = kmTraveled!! / 100
+            val costForTravel = fuelPrice!! * requiredAmountOfFuel
+            val costPerPerson = costForTravel / numberOfPeople!!
+
+            calculatorEventChannel.send(CalculatorEvent.ShowResultForCostsTab(requiredAmountOfFuel, costForTravel, costPerPerson))
         }
     }
 
     private fun doCalculationInRangeTab() = viewModelScope.launch {
         if (avgFuelConsumption == null || paid == null || fuelPrice == null) {
             calculatorEventChannel.send(CalculatorEvent.ShowMessageAboutMissingData)
+        } else {
+            val amountOfFilledWithFuel = paid!! / fuelPrice!!
+            val rangeInKm = amountOfFilledWithFuel / avgFuelConsumption!! * 100
+
+            calculatorEventChannel.send(CalculatorEvent.ShowResultForRangeTab(amountOfFilledWithFuel, rangeInKm))
         }
     }
 
     sealed class CalculatorEvent {
         object ShowMessageAboutMissingData : CalculatorEvent()
+        data class ShowResultForConsumptionTab(val avgConsumption: Double, val price: Double) : CalculatorEvent()
+        data class ShowResultForCostsTab(val requiredAmountOfFuel: Double, val costForTravel: Double, val costPerPerson: Double) : CalculatorEvent()
+        data class ShowResultForRangeTab(val amountOfFilledWithFuel: Double, val rangeInKm: Double) : CalculatorEvent()
     }
 
 }
